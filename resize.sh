ca388b745d490e7db95d01ffb9a617ce40626fc5
befovy <befovy@gmail.com>
2019-12-06 17:20:11 +0800

    resize

---
+++ resize.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+git checkout --orphan latest_branch
+git add -A
+git commit -am "resize"
+git branch -D master
+git branch -m master
+
+git fsck
+git reflog expire --expire-unreachable=now --all
+git gc --prune=now
+
+git push --set-upstream origin master
+git push -f origin master
